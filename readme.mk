9854a5e39919beadbb63fe3fbc6ca5134a73c533
par551 <par551@cs.bham.ac.uk>
2017-01-22 05:00:17 +0000

    Added a basic readme file.

---
+++ readme.mk
@@ -0,0 +1,14 @@
+The Small State Institute
+=========================
+
+A portal of policy proposals for future and current right-wing politicians. An open-source in development think-tank, seeking to provide web-applications and algorithms to facilitate a further shrinking of the state.
+
+The current layout of the website is the homepage, 3 web applications and 4 overarching policy areas to be developed in the future.
+
+HOME
+ - Deficit Reduction
+   - An interactive online calculator listing numerous ways by which the UK government can reduce expenditure, with the ability to add & remove key assumptions.
+ - Electoral Analysis
+   - A visualisation tool for searching MPs and Constituencies.
+ - Boundary Reform
+   - An algorithmic tool examining the feasibility of automating boundary changes, nomenclature aside, so as to prevent any risk of gerrymandering ocurring.
